924058194b3bd17ce7f86b50c93a30e254ff999a
sense <rogers@hibobi.com>
2022-03-29 19:58:27 +0800

    feat: remove pkg

--- internal/data/dingtalk.go
+++ internal/data/dingtalk.go
@@ -2,18 +2,57 @@ package data
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
 	"time"
 
+	util "github.com/alibabacloud-go/tea-utils/service"
+
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	"github.com/alibabacloud-go/dingtalk/contact_1_0"
+	"github.com/alibabacloud-go/dingtalk/oauth2_1_0"
+	"github.com/alibabacloud-go/tea/tea"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-redis/redis/v8"
 	v1 "github.com/rogerogers/dingtalk-ops/api/helloworld/v1"
 	"github.com/rogerogers/dingtalk-ops/internal/biz"
-	"github.com/rogerogers/dingtalk-ops/pkg/dingtalk"
 )
+
+var (
+	client        *oauth2_1_0.Client
+	once          = &sync.Once{}
+	contactClient *contact_1_0.Client
+)
+
+func init() {
+	var _err error
+	once.Do(func() {
+		config := &openapi.Config{}
+		config.Protocol = tea.String("https")
+		config.RegionId = tea.String("central")
+		client, _err = oauth2_1_0.NewClient(config)
+		if _err != nil {
+			log.Error(_err)
+		}
+		contactClient, _err = contact_1_0.NewClient(config)
+		if _err != nil {
+			log.Error(_err)
+		}
+	})
+}
 
 type dingtalkRepo struct {
 	data *Data
 	log  *log.Helper
+}
+
+type UserInfoResult struct {
+	Result v1.GetUserInfoByUserIdReply `json:"result"`
 }
 
 // NewGreeterRepo .
@@ -30,7 +69,7 @@ func (r *dingtalkRepo) GetDingtalkToken() (string, error) {
 	key := "hibobi_ops:dingtalk-token"
 	getValue, err := r.data.Cache.Get(cacheCtx, key).Result()
 	if err == redis.Nil {
-		token := dingtalk.GetAccessToken()
+		token := GetAccessToken()
 		r.data.Cache.Set(cacheCtx, key, token, 3600*time.Second)
 		return token, nil
 	} else if err != nil {
@@ -41,7 +80,7 @@ func (r *dingtalkRepo) GetDingtalkToken() (string, error) {
 }
 
 func (r *dingtalkRepo) GetUserToken(ctx context.Context, d *v1.GetUserTokenRequest) (*v1.GetUserTokenReply, error) {
-	result, err := dingtalk.GetUserToken(d.AuthCode)
+	result, err := GetUserToken(d.AuthCode)
 	if err != nil {
 		return &v1.GetUserTokenReply{}, err
 	} else {
@@ -51,19 +90,122 @@ func (r *dingtalkRepo) GetUserToken(ctx context.Context, d *v1.GetUserTokenReque
 	}
 }
 
+//GetAccessToken 获取企业access_token
+func GetAccessToken() (token string) {
+	getAccessTokenRequest := &oauth2_1_0.GetAccessTokenRequest{
+		AppKey:    tea.String(os.Getenv("DING_KEY")),
+		AppSecret: tea.String(os.Getenv("DING_SECRET")),
+	}
+	token, tryErr := func() (token string, _e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		res, _err := client.GetAccessToken(getAccessTokenRequest)
+		if _err != nil {
+			return "", _err
+		}
+		return *res.Body.AccessToken, nil
+	}()
+
+	if tryErr != nil {
+		var err = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			err = _t
+		} else {
+			err.Message = tea.String(tryErr.Error())
+		}
+		if !tea.BoolValue(util.Empty(err.Code)) && !tea.BoolValue(util.Empty(err.Message)) {
+			// err 中含有 code 和 message 属性，可帮助开发定位问题
+			log.Error(err)
+		}
+	}
+	return token
+}
+
+//GetUserToken 获取用户token
+func GetUserToken(code string) (token string, err error) {
+	getUserTokenRequest := oauth2_1_0.GetUserTokenRequest{
+		ClientId:     tea.String(os.Getenv("DING_KEY")),
+		ClientSecret: tea.String(os.Getenv("DING_SECRET")),
+		Code:         tea.String(code),
+		GrantType:    tea.String("authorization_code"),
+	}
+	result, err := client.GetUserToken(&getUserTokenRequest)
+	if err != nil {
+		return "", err
+	} else {
+		return *result.Body.AccessToken, nil
+	}
+}
+
+func GetUserInfoByToken(token string) *string {
+	header := contact_1_0.GetUserHeaders{
+		XAcsDingtalkAccessToken: &token,
+	}
+	unionId := "me"
+	res, err := contactClient.GetUserWithOptions(&unionId, &header, &util.RuntimeOptions{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	return res.Body.UnionId
+}
+
 func (r *dingtalkRepo) GetUserInfoByToken(ctx context.Context, d *v1.GetUserInfoByTokenRequest) (*v1.GetUserInfoByTokenReply, error) {
 	return &v1.GetUserInfoByTokenReply{}, nil
 }
+
+func GetUserIdByUnionId(accessToken string, unionid string) (userId string, err error) {
+	requestBody, err := json.Marshal(map[string]string{"unionid": unionid})
+	if err != nil {
+		return "", err
+
+	}
+	res, err := http.Post(fmt.Sprintf("https://oapi.dingtalk.com/topapi/user/getbyunionid?access_token=%s", accessToken), "application/json", strings.NewReader(string(requestBody)))
+	if err != nil {
+		return "", err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(resBody), nil
+
+}
+
 func (r *dingtalkRepo) GetUserIdByUnionId(ctx context.Context, d *v1.GetUserIdByUnionIdRequest) (*v1.GetUserIdByUnionIdReply, error) {
 	return &v1.GetUserIdByUnionIdReply{}, nil
 }
 
+func GetUserInfoByUserId(accessToken string, userId string) (userInfoRes *UserInfoResult, err error) {
+	requestBody, err := json.Marshal(map[string]string{"userid": userId})
+	if err != nil {
+		return &UserInfoResult{}, err
+	}
+	res, err := http.Post(fmt.Sprintf("https://oapi.dingtalk.com/topapi/v2/user/get?access_token=%s", accessToken), "application/json", strings.NewReader(string(requestBody)))
+
+	if err != nil {
+		return &UserInfoResult{}, err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return &UserInfoResult{}, err
+	}
+	err = json.Unmarshal(resBody, &userInfoRes)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
+//GetUserInfoByUserId 根据用户id获取用户信息
 func (r *dingtalkRepo) GetUserInfoByUserId(ctx context.Context, d *v1.GetUserInfoByUserIdRequest) (*v1.GetUserInfoByUserIdReply, error) {
 	token, err := r.GetDingtalkToken()
 	if err != nil {
 		r.log.Error(err)
 	}
-	result, err := dingtalk.GetUserInfoByUserId(token, d.UserId)
+	result, err := GetUserInfoByUserId(token, d.UserId)
 	if err != nil {
 		return &v1.GetUserInfoByUserIdReply{}, err
 	} else {
